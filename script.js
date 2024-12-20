07ca12ce44a91792096575ddde8ffca4e9c86ff0
fabiodesteves <fabiodesteves@gmail.com>
2024-12-20 19:59:13 +0000

    Create function to play a round of the game
    Create the logic to play one single round,
    comparing user choices against computer choices.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create a function to select a computer option
-function getComputerOption() {
+function getComputerChoice() {
   let randomInt = Math.floor(Math.random() * 3); // returns 0, 1, or 2
   if (randomInt === 0) {
     return "rock";
@@ -14,9 +14,40 @@ function getUserChoice() {
   let userChoice = prompt("Rock, paper, or scissors?");
   const options = ["rock", 'paper', 'scissors'];
   if (options.indexOf(userChoice.toLowerCase()) === -1) {
-    alert("not a valid option. Please select rock, paper, or scissors")
+    alert("not a valid option. Please select rock, paper, or scissors");
   } else {
-    return userChoice.toLowerCase()
+    return userChoice.toLowerCase();
   }
 }
-// Create a function to test user option vs computer option
+
+// Initialise computer and user scores with 0
+let computerScore = 0;
+let userScore = 0;
+
+let userChoice = getUserChoice();
+let computerChoice = getComputerChoice();
+
+// Create a function to play a round
+function playRound(userChoice, computerChoice) {
+  if ((userChoice == "rock") && (computerChoice == "paper")) {
+    computerScore++;
+    alert(`You lose. ${computerChoice} beats ${userChoice}.`);
+  } else if ((userChoice == "paper") && (computerChoice == "scissors")) {
+    computerScore++;
+    alert(`You lose. ${computerChoice} beats ${userChoice}.`);
+  } else if ((userChoice == "scissors") && (computerChoice == "rock")) {
+    computerScore++;
+    alert(`You lose. ${computerChoice} beats ${userChoice}.`);
+  } else if ((userChoice == "rock") && (computerChoice == "scissors")) {
+    userScore++;
+    alert(`You win! ${userChoice} beats ${computerChoice}.`);
+  } else if ((userChoice == "paper") && (computerChoice == "rock")) {
+    userScore++;
+    alert(`You win! ${userChoice} beats ${computerChoice}.`);
+  } else if ((userChoice == "scissors") && (computerChoice == "paper")) {
+    userScore++;
+    alert(`You win! ${userChoice} beats ${computerChoice}.`);
+  } else if (userChoice == computerChoice) {
+    alert("It's a tie!")
+  }
+}
