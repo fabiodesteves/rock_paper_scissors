8b8d0bac74d60f07cebdde914a51078cf331b24a
fabiodesteves <fabiodesteves@gmail.com>
2024-12-20 19:39:22 +0000

    Create "getUserChoice" function
    Create function that prompts user to select
    "rock", "paper" or "scissors".
    Verify whether the selected option is valid.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log('Hello world!');
-
 // Create a function to select a computer option
 function getComputerOption() {
   let randomInt = Math.floor(Math.random() * 3); // returns 0, 1, or 2
@@ -12,4 +10,13 @@ function getComputerOption() {
   }
 }
 // Create a function to get a user option
+function getUserChoice() {
+  let userChoice = prompt("Rock, paper, or scissors?");
+  const options = ["rock", 'paper', 'scissors'];
+  if (options.indexOf(userChoice.toLowerCase()) === -1) {
+    alert("not a valid option. Please select rock, paper, or scissors")
+  } else {
+    return userChoice.toLowerCase()
+  }
+}
 // Create a function to test user option vs computer option
